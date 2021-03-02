c02d75afb611f22edb27cc3dd6fadd1c5f325e11
Alex Berson <aberson@covermymeds.com>
2021-03-01 19:17:51 -0500

    Created Extension

---
+++ README.md
@@ -0,0 +1,7 @@
+# search-all-cases
+
+Uses regex and findInFiles to search for the selected text with multiple cases.
+
+From Command Options (command+shift+P): `Search All Cases`
+
+Actual command: `search-all-cases.search`
---
+++ src/extension.ts
@@ -0,0 +1,40 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { camelCase, pascalCase, snakeCase } from 'change-case';
+
+interface IFindInFilesArgs {
+	query?: string;
+	replace?: string;
+	preserveCase?: boolean;
+	triggerSearch?: boolean;
+	filesToInclude?: string;
+	filesToExclude?: string;
+	isRegex?: boolean;
+	isCaseSensitive?: boolean;
+	matchWholeWord?: boolean;
+	useExcludeSettingsAndIgnoreFiles?: boolean;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('search-all-cases.search', () => {
+		const editor = vscode.window.activeTextEditor
+		const selection = editor?.selection
+		const text = editor?.document.getText(selection)
+		if (text == undefined) {
+			console.log("No selected text")
+			return;
+		}
+		const searchText = `${text}|${camelCase(text)}|${pascalCase(text)}|${snakeCase(text)}`
+		let params: IFindInFilesArgs = {
+			query: searchText,
+			isRegex: true,
+			triggerSearch: true
+		}
+
+		vscode.commands.executeCommand('workbench.action.findInFiles', params)
+	});
+
+	context.subscriptions.push(disposable);
+}
+export function deactivate() {}
